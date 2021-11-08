4ce118cf1074614cb48f41790d84a61b2190bae5
cameron04 <89870376+cameron04@users.noreply.github.com>
2021-11-07 21:30:49 -0600

    Update deflection.m

--- deflection.m
+++ deflection.m
@@ -1,31 +1,111 @@
-function [v0,w0] = deflection(Mz,My,Iyy,Izz,Iyz,Er,x0)
+function deflection_matrix = deflection(Pmax,Iyy,Izz,Iyz,Er,A_star,P,x0)
 
 I_tilde = Iyy * Izz - Iyz^2;
+My = 0;
 
+%%Case 1: P == 0---------------------------------------------------------
 %deflection in the y direction
-syms x dv(x) v(x)
-v0_dp = (1/(Er * I_tilde)) * (Mz * Iyy + My * Iyz);
+syms x v0_dp1 dv1 v1
+Mz_0(x) = 5 * (x - x0);
+v0_dp1 = (1/(Er * I_tilde)) * (Mz_0 * Iyy + My * Iyz);
 
-dv(x) = int(v0_dp);
-c1 = -1 * dv(0);
-dv(x) = dv(x) + c1;
+dv1(x) = int(v0_dp1);
+c1 = -1 * dv1(0);
+dv1(x) = dv1(x) + c1;
 
-v(x) = int(dv);
-c2 = -1 * v(0);
-v(x) = v(x) + c2;
-
-v0 = v(x0);
+v1(x) = int(dv1);
+c2 = -1 * v1(0);
+v1(x) = v1(x) + c2;
 
 %deflection in the z direction
-syms x dw(x) w(x)
-w0_dp = (-1/(Er * I_tilde)) * (Mz * Iyz + My * Izz);
+syms x wo_dp1 dw1 w1
+w0_dp1 = (-1/(Er * I_tilde)) * (Mz_0 * Iyz + My * Izz);
 
-dw(x) = int(w0_dp);
-c1 = -1 * dw(0);
-dw(x) = dw(x) + c1;
+dw1(x) = int(w0_dp1);
+c1 = -1 * dw1(0);
+dw1(x) = dw1(x) + c1;
 
-w(x) = int(dw);
-c2 = -1 * w(0);
-w(x) = w(x) + c2;
+w1(x) = int(dw1);
+c2 = -1 * w1(0);
+w1(x) = w1(x) + c2;
 
-w0 = w(x0);
+%deflection in the x direction
+syms x du1 u1
+du1(x) = P/(Er * A_star);
+
+u1(x) = int(du1);
+c1 = -1 * u1(0);
+u1(x) = u1(x) + c1;
+
+%%Case 2: P == 15---------------------------------------------------------
+%deflection in the y direction
+syms x v0_dp2 dv2 v2
+Mz_15(x) = -10 * (x - x0);
+v0_dp2 = (1/(Er * I_tilde)) * (Mz_15 * Iyy + My * Iyz);
+
+dv2(x) = int(v0_dp2);
+c1 = -1 * dv2(0);
+dv2(x) = dv2(x) + c1;
+
+v2(x) = int(dv2);
+c2 = -1 * v2(0);
+v2(x) = v2(x) + c2;
+
+%deflection in the z direction
+syms x wo_dp2 dw2 w2
+w0_dp2 = (-1/(Er * I_tilde)) * (Mz_15 * Iyz + My * Izz);
+
+dw2(x) = int(w0_dp2);
+c1 = -1 * dw2(0);
+dw2(x) = dw2(x) + c1;
+
+w2(x) = int(dw2);
+c2 = -1 * w2(0);
+w2(x) = w2(x) + c2;
+
+%deflection in the x direction
+syms x du2 u2
+du2(x) = P/(Er * A_star);
+
+u2(x) = int(du2);
+c1 = -1 * u2(0);
+u2(x) = u2(x) + c1;
+
+%%Case 1: P == Pmax---------------------------------------------------------
+%deflection in the y direction
+syms x v0_dp3 dv3 v3
+Mz_max(x) = (-Pmax + 5) * (x - x0);
+v0_dp3 = (1/(Er * I_tilde)) * (Mz_max * Iyy + My * Iyz);
+
+dv3(x) = int(v0_dp3);
+c1 = -1 * dv3(0);
+dv3(x) = dv3(x) + c1;
+
+v3(x) = int(dv3);
+c2 = -1 * v3(0);
+v3(x) = v3(x) + c2;
+
+%deflection in the z direction
+syms x wo_dp3 dw3 w3
+w0_dp3 = (-1/(Er * I_tilde)) * (Mz_max * Iyz + My * Izz);
+
+dw3(x) = int(w0_dp3);
+c1 = -1 * dw3(0);
+dw3(x) = dw3(x) + c1;
+
+w3(x) = int(dw3);
+c2 = -1 * w3(0);
+w3(x) = w3(x) + c2;
+
+%deflection in the x direction
+syms x du3 u3
+du3(x) = P/(Er * A_star);
+
+u3(x) = int(du3);
+c1 = -1 * u3(0);
+u3(x) = u3(x) + c1;
+%------------------------------------------------------------------------
+%%Deflection matrix assembly
+deflection_matrix = [u1(x0) v1(x0) w1(x0);
+    u2(x0) v2(x0) w2(x0);
+    u3(x0) v3(x0) w3(x0)];
