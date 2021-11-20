8ce06199eba644a815f1da8c83215c66b61db109
FlyAeroMan <93842887+FlyAeroMan@users.noreply.github.com>
2021-11-20 16:48:43 -0600

    created bucklingcalc.m updated findpmax.m
    bucklingcalc.m:
    - uses an equation that I derived to calculate the rib spacing
    - trying to get it to calculate the force in the skin and it doesn't appear to be calculating correctly
    
    findpmax.m:
    - realized the units were wrong on the yield strengths and moduli of elasticity.
    - values now seem really high... perhaps this is assuming no buckling?

---
+++ src/bucklingcalc.m
@@ -0,0 +1,22 @@
+function riblocal = bucklingcalc(alldata,pmax)
+
+
+% Obtain values from alldata
+tskin = 0.0625; %BUGBUG-- should retrieve from alldata
+Xo = alldata{5}(2,10);
+Etop = alldata{1}(1,1)*1000; %(psi)
+Ebot = alldata{1}(2,1)*1000; %(psi)
+ER = alldata{2}(7)*1000; %    (psi)
+Itilda_star = alldata{2}(6);
+Iyy_star = alldata{2}(3);
+Iyz_star = alldata{2}(5);
+ybar = abs(alldata{3}(1));
+zbar = abs(alldata{3}(2));
+lengthtop = alldata{4}(1,3)-alldata{4}(2,3);
+
+% initialize xprime for later
+xprime = Xo;
+
+% Calculate force in Top Skin:
+Ftopskin = ((tskin*((pmax-5)*xprime)*Etop)/(ER*Itilda_star))*(Iyz_star*((lengthtop^2/2)-(3.75-zbar)*lengthtop)-ybar*Iyy_star*lengthtop);
+end
