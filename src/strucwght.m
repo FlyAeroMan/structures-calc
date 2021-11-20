57d5c39238500a57ca47b5fae55746af885e2191
McClure99 <jshmcclure@gmail.com>
2021-11-20 17:59:17 -0600

    Create strucwght.m
    Calculates the total weight of the structure as well as the weight of individual pieces
    Closes #3

---
+++ src/strucwght.m
@@ -0,0 +1,15 @@
+function weight = strucwght(alldata)
+%estimates structure weight before glue
+
+a = alldata{1,1};%pulls the needed data matrix out of the alldata matrix
+
+specwght = a(:,6);%stores all of the specwght values from the a matrix
+vol = a(:,7);%stores the values of volume for each element
+weight(1,1) = 0;
+for i = 1:length(specwght)%calculates the total weight and the weight of each component
+    
+    weight(1,1) = weight(1,1) + (vol(i,1)/1728)*specwght(i,1);%calculates total weight
+    
+    weight(i,1) = (vol(i,1)/1728)*specwght(i,1);%calculates weight of individual pieces
+end
+end
