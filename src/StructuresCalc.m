3dd15ca210f767e522d1a4c92275f0abf3648243
FlyAeroMan <93842887+FlyAeroMan@users.noreply.github.com>
2021-11-21 17:43:26 -0600

    Updated bucklingcalc.m and added call in StructuresCalc.m
    bucklingcalc.m:
    - added rudimentary title block, still missing info
    - now alerts user of progress
    - now iterates to find the space between each rib and its neighbor closer to the fixed end.
    - oh and it can now calculate the distance between a rib and its neighbor (improvements yay!)
    - rounds the true calculated value down to the nearest 1/4 inch for a safety factor >1.
    - the only problem is that many values are hardcoded, and it only considers the top skin buckling for now, I will add the bottom skin calculations later. then it can compare the two and pick the one with closer spacing to display to the user.
    - Note to self: we need more stringers on the skin to reduce the number of ribs.

--- src/StructuresCalc.m
+++ src/StructuresCalc.m
@@ -36,6 +36,9 @@ alldata = datafromsheet;
 % Calculate the stress at pmax
 stressmax = stresscalc(alldata,stressmaxlocal,pmax);
 
+% Calculate Rib Spacing
+riblocal = bucklingcalc(alldata,pmax);
+
 % Calculate Deflection
 %[u,v,w] = deflection(input vars);
 
--- src/bucklingcalc.m
+++ src/bucklingcalc.m
@@ -1,5 +1,10 @@
 function riblocal = bucklingcalc(alldata,pmax)
+% bucklingcalc - a function to calculate the rib spacing to prevent
+% buckling of the structure until the predicted failure load pmax
+%FORMAT: riblocal = bucklingcalc(alldata,pmax)
 
+% Alert User of Progress
+fprintf('Determining Rib spacing...\n')
 
 % Obtain values from alldata
 tskin = 0.0625; %BUGBUG-- should retrieve from alldata
@@ -22,7 +27,9 @@ yTE = abs(ybar)+(tskin/2);
 
 % initialize xprime for later
 xprime = Xo;
+itr = 1;
 
+while xprime > 1
 % Calculate the bending stress at the LE and TE of the top skin
 sigmaxxLE = (Etop/(ER*Itilda_star))*(-pmax+5)*(-xprime)*(zLE*Iyz_star-yLE*Iyy_star);
 sigmaxxTE = (Etop/(ER*Itilda_star))*(-pmax+5)*(-xprime)*(zTE*Iyz_star-yTE*Iyy_star);
@@ -33,16 +40,44 @@ Ftopskin = tskin*lengthtop*((sigmaxxLE + sigmaxxTE)/2);
 %Ftopskin2 = ((tskin*((pmax-5)*xprime)*Etop)/(ER*Itilda_star))*(Iyz_star*((lengthtop^2/2)-(3.5-zbar)*lengthtop)-ybar*Iyy_star*lengthtop);
 
 % Hardcoding the centroid location of the stringers
-yTopStringercentroid = abs(ybar)+0.25;
-zTopStringercentroid = abs(zbar)-0.0625;
+%yTopStringercentroid = abs(ybar)+0.25;
+%zTopStringercentroid = abs(zbar)-0.0625;
 
 % Calculating stress at the centroid
-sigmaxxTopStringer = (Etop/(ER*Itilda_star))*(-pmax+5)*(-xprime)*(zTopStringercentroid*Iyz_star-yTopStringercentroid*Iyy_star);
+%sigmaxxTopStringer = (Etop/(ER*Itilda_star))*(-pmax+5)*(-xprime)*(zTopStringercentroid*Iyz_star-yTopStringercentroid*Iyy_star);
 
 % Calculate force in stringer:
-Ftopstringer = sigmaxxTopStringer*0.125*0.375;
+%Ftopstringer = sigmaxxTopStringer*0.125*0.375;
+Ftopstringer = 0;
 
-% calculate Nx and Ncr
+% Calculate Nx
 Nx = (1/lengthtop)*(Ftopskin+Ftopstringer);
-%Ncr = 
+
+% Calculate a, the spacing between the ribs in inches
+stringercount = 0; %BUGBUG-- Hardcoded Values
+stringerwidth = 0.125; %BUGBUG-- Hardcoded Values
+s = lengthtop/(stringercount +1);
+x = (((12*Nx*lengthtop^4)*(s-stringerwidth+stringerwidth*(tskin/(tskin+stringerwidth))^3))/(pi^2*Etop*s*tskin^3))-2*lengthtop^2;
+v = lengthtop^4;
+aone = sqrt((-x+sqrt(x^2-4*v))/(2));
+atwo = sqrt((-x-sqrt(x^2-4*v))/(2));
+
+% Determine the smaller value of a and call it ribspacing
+if aone < atwo
+    temp = aone*4;
+    temp = round(temp)/4;
+    riblocal(itr) = temp;
+    xprime = xprime - temp;
+else
+    temp = atwo*4;
+    temp = round(temp)/4;
+    riblocal(itr) = temp;
+    xprime = xprime - temp;
+end
+itr = itr + 1;
+end
+
+%Alert User of Progress
+fprintf('Rib Spacing based off of top surface determined.\n')
+
 end
