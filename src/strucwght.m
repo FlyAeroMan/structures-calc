17b22eb289191e1174a70f8eeead8039c89da78f
McClure99 <93931931+McClure99@users.noreply.github.com>
2021-11-20 19:49:38 -0600

    Update strucwght.m

--- src/strucwght.m
+++ src/strucwght.m
@@ -7,9 +7,10 @@ specwght = a(:,6);%stores all of the specwght values from the a matrix
 vol = a(:,7);%stores the values of volume for each element
 weight(1,1) = 0;
 for i = 1:length(specwght)%calculates the total weight and the weight of each component
+    if ~isnan(vol(i,1)) && ~isnan(specwght(i,1)) 
+        weight(1,1) = weight(1,1) + (vol(i,1)/1728)*specwght(i,1);%calculates total weight
     
-    weight(1,1) = weight(1,1) + (vol(i,1)/1728)*specwght(i,1);%calculates total weight
-    
-    weight(i,1) = (vol(i,1)/1728)*specwght(i,1);%calculates weight of individual pieces
+        weight(i,1) = (vol(i,1)/1728)*specwght(i,1);%calculates weight of individual pieces
+    end
 end
 end
