5e9516b38bf0bf3978fb61051e637e81f2137a88
FlyAeroMan <93842887+FlyAeroMan@users.noreply.github.com>
2021-11-22 21:21:33 -0600

    Update bucklingcalc.m
    Added Copyright block to title block

--- src/bucklingcalc.m
+++ src/bucklingcalc.m
@@ -3,6 +3,28 @@ function riblocal = bucklingcalc(alldata,pmax)
 % buckling of the structure until the predicted failure load pmax
 %FORMAT: riblocal = bucklingcalc(alldata,pmax)
 
+% Copyright (C) 2021  Kale Macormic
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+% November 22, 2021 09:19:39 PM CST
+
+% NOTICE: This source code is Copyright (C) 2021  Kale Macormic and is
+% intended for AE525 Instructor/Grader use only. If you obtain this
+% document in error destroy it immediately.
+
+
 % Alert User of Progress
 fprintf('Determining Rib spacing...\n')
 
